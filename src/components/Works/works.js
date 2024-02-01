f6935530019f0329281fc6b451c58bc6f07039d9
Tõnis <tonislille@gmail.com>
2024-02-01 17:42:47 +0200

    Update My Porfolio

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -3,45 +3,64 @@ import './works.css';
 import calkulatorApp from '../../assets/calculatorAppIcon.png';
 import gymAppIcon from '../../assets/gymAppIcon.png'
 import projectSiteIcon from '../../assets/portfolioProjectIcon.png'
-import jobScrapeIcon from '../../assets/JobScrapeIcon.png'
+import jobScrapeIcon from '../../assets/webScrapePic02.png'
+import ProjectHouseIcon from '../../assets/ProjectHouseIcon02.png'
 
 const Works = () => {
     return (
         <section id='works'>  
             <h2 className='worksTitle'> My Portfolio</h2>
-            <span className='worksDesc'>Featured projects!</span>
+            <span className='worksDescription'>Featured projects!</span>
             <div className='worksLists'>
 
                  <div className='worksList'>
-                    <img src={calkulatorApp} alt="calkulatorApp" className='worksListImg'/>
-                    <div className='worksListText'>
-                    <h2><a href='https://github.com/t6nislille/ScientificCalculator' target='_blank' rel='noopener noreferrer'>Scientific Calculator</a></h2>
-                        <p> .MAUI app, developed with C# and XML. </p>
+                    <div className='worksContent'>
+                        <img src={calkulatorApp} alt="calkulatorApp" className='worksListImg'/>
+                        <div className='worksDescription'>
+                            <p> .NET MAUI Calculator app <br></br> Developed with: C# and XML </p>
+                        </div>
                     </div>
+                    <a href="https://github.com/t6nislille" target="_blank" rel="noopener noreferrer" className="projectButton">Github</a>
                 </div>
 
                 <div className='worksList'>
-                    <img src={gymAppIcon} alt="gymAppIcon" className='worksListImg'/>
-                    <div className='worksListText'>
-                    <h2><a href='https://github.com/t6nislille/ProjectSsentifGenie' target='_blank' rel='noopener noreferrer'>Gym Helper</a></h2>
-                        <p> .MAUI app, developed with C# and XML. </p>
-                        
+                    <div className='worksContent'>
+                        <img src={gymAppIcon} alt="gymAppIcon" className='worksListImg'/>
+                        <div className='worksDescription'>
+                            <p> .NET MAUI Exercise app, to help beginners use correct forms while working out.<br></br> Developed with C# and XML. </p>     
+                        </div>                   
                     </div>
+                    <a href="https://github.com/t6nislille/ProjectSsentifGenie" target="_blank" rel="noopener noreferrer" className="projectButton">Github</a>
                 </div>
 
                 <div className='worksList'>
-                    <img src={jobScrapeIcon} alt="jobScrapeIcon" className='worksListImg'/>
-                    <div className='worksListText'>
-                    <h2><a href='https://github.com/t6nislille/Job-List-Scraping' target='_blank' rel='noopener noreferrer'>Job Site Scraper</a></h2>
-                        <p> Developed with Python </p>
+                    <div className='worksContent'>
+                        <img src={jobScrapeIcon} alt="jobScrapeIcon" className='worksListImg'/>
+                        <div className='worksDescription'>
+                            <p>Program to gather job applications from CVkeskus.ee <br></br> Developed with Python </p>
+                        </div>
                     </div>
+                    <a href="https://github.com/t6nislille/Job-List-Scraping" target="_blank" rel="noopener noreferrer" className="projectButton">Github</a>
+
                 </div>
 
                 <div className='worksList'>
-                    <img src={projectSiteIcon} alt="projectSiteIcon" className='worksListImg'/>
-                    <div className='worksListText'>
-                    <h2><a href='https://github.com/t6nislille/Job-List-Scraping' target='_blank' rel='noopener noreferrer'>Portfolio Website</a></h2>
-                        <p> Developed with ReactJS </p>
+                    <div className='worksContent'>
+                       <img src={projectSiteIcon} alt="projectSiteIcon" className='worksListImg'/>
+                        <div className='worksDescription'>
+                            <p> Portfolio Website to bring forward my skills and some of my projects<br></br> Developed with ReactJS </p>
+                        </div>
+                    </div>
+                    <a href="https://github.com/t6nislille/Portfolio-Website" target="_blank" rel="noopener noreferrer" className="projectButton">Github</a>
+                </div>
+
+                <div className='worksList'>
+                    <div className='worksContent'>
+                        <img src={ProjectHouseIcon} alt="ProjectHouseIcon" className='worksListImg'/>
+                        <div className='worksDescription'>
+                            <p> .NET real estate CRUD with a database connection <br></br> Developed with C#,<br></br> Database connection with MySQL</p>
+                        </div>
+                        <a href="https://github.com/t6nislille/House" target="_blank" rel="noopener noreferrer" className="projectButton">Github</a>
                     </div>
                 </div>
 
