2a127f200c8dda4bf5a7af9e0d296c28a89920e1
Tõnis <tonislille@gmail.com>
2025-10-09 15:40:57 +0300

    Import Swiper styles

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -1,7 +1,13 @@
 import React from 'react'
-// import Swiper core and required modules
+// Import Swiper core and required modules
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 import './works.css';
 import calkulatorApp from '../../assets/calculatorAppIcon.png';
 import gymAppIcon from '../../assets/gymAppIcon.png'
