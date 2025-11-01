b52de005425cde151a5cc6960e2eac6c4bf76edd
Tõnis <tonislille@gmail.com>
2025-11-01 17:54:25 +0200

    Remove Works from homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Navbar from "./components/NavBar/navbar";
 import Intro from "./components/Intro/intro.js";
 import Skills from "./components/Skills/skills.js";
-import Works from "./components/Works/works.js";
 import Contact from "./components/Contact/contact.js";
 import Footer from "./components/Footer/footer.js";
 import Carousell from "./components/Carousell/Carousell.js"
@@ -13,9 +12,8 @@ function App() {
     <div className="App">
       <Navbar/>
       <Intro/>
-      <Skills/>
       <Carousell/>
-      <Works/>
+      <Skills/>
       <Contact/>
       <Footer/>
     </div>
